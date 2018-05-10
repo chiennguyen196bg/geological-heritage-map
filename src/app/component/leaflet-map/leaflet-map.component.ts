0bc4992d9baaa24b9bbc8a3631ed9f9602e12fda
chien nguyen <chienminhnguyen196@gmail.com>
2018-05-11 00:44:51 +0700

    change marker icon, zoom, etc

--- src/app/component/leaflet-map/leaflet-map.component.ts
+++ src/app/component/leaflet-map/leaflet-map.component.ts
@@ -26,15 +26,10 @@ export class LeafletMapComponent implements OnInit, OnChanges {
       layers: [
         tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
       ],
-      zoom: 5,
+      zoom: 10,
       center: latLng(12.4587489, 107.9188864)
     };
-    console.log(this.data);
-    this.layers = [
-      // circle([ 46.95, -122 ], { radius: 5000 }),
-      // polygon([[ 46.8, -121.85 ], [ 46.92, -121.92 ], [ 46.87, -121.8 ]]),
-      marker([46.879966, -121.726909])
-    ];
+    // console.log(this.data);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -45,10 +40,11 @@ export class LeafletMapComponent implements OnInit, OnChanges {
         const coordinates = item.geometry.coordinates;
         return marker(latLng(coordinates[1], coordinates[0]), {
           icon: icon({
-            iconSize: [25, 41],
-            iconAnchor: [13, 41],
-            iconUrl: 'assets/marker-icon.png',
-            shadowUrl: 'assets/marker-shadow.png'
+            // iconSize: [25, 41],
+            // iconAnchor: [13, 41],
+            // iconUrl: 'assets/marker-icon.png',
+            // shadowUrl: 'assets/marker-shadow.png',
+            iconUrl: 'assets/images/DiemDiSan_0_8.png'
           })
         }).on('click', () => {
           console.log('click on marker: ' + item.sign);
