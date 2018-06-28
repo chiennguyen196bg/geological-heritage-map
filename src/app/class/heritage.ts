b0cbc41a1008e458b0b1726d887bb95b4ffef15a
chien nguyen <chienminhnguyen196@gmail.com>
2018-06-29 00:09:44 +0700

    done export excel

--- src/app/class/heritage.ts
+++ src/app/class/heritage.ts
@@ -1,5 +1,5 @@
 export class Heritage {
-    id: string;
+    id: number;
     name: string;
     type: string;
     label: string;
--- src/app/service/excel.service.ts
+++ src/app/service/excel.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
+import { Heritage } from '../class/heritage';
 
-const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
 const EXCEL_EXTENSION = '.xlsx';
 
 @Injectable()
@@ -10,19 +9,25 @@ export class ExcelService {
 
   constructor() { }
 
-  public exportAsExcelFile(json: any[], excelFileName: string): void {
+  public exportAsExcelFile(json: Heritage[], excelFileName: string): void {
     console.log(json);
-    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+    const data = json.sort((a, b) => (a.id - b.id)).map((item => this.convertHeritage(item)));
+    const fileName = excelFileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION;
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data);
     const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
-    this.saveAsExcelFile(excelBuffer, excelFileName);
+    XLSX.writeFile(workbook, fileName, { bookType: 'xlsx', type: 'buffer' });
   }
 
-  private saveAsExcelFile(buffer: any, fileName: string): void {
-    const data: Blob = new Blob([buffer], {
-      type: EXCEL_TYPE
-    });
-    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+  private convertHeritage(heritage: Heritage): any {
+    return {
+      'ID': heritage.id,
+      'Tên': heritage.name,
+      'Kiểu': heritage.type,
+      'Ký hiệu': heritage.label,
+      'Huyện': heritage.district,
+      'Xã': heritage.commune,
+      'Kinh độ': heritage.geometry.coordinates[1],
+      'Vĩ độ': heritage.geometry.coordinates[0]
+    }
   }
-
 }
